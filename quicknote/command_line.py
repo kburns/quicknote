c64f840af723dca404ea7d14432ff9b3232716ab
Keaton J. Burns <keaton.burns@gmail.com>
2021-04-22 11:28:22 -0400

    Make default title based on filename

--- quicknote/command_line.py
+++ quicknote/command_line.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-import jinja2 
+import jinja2
 import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", metavar='filename', type=str, help="name of the file.")
-    parser.add_argument("-d", "--doctype", dest='doctype', help = "Type of note. Options are teaching, research.", default="teaching")
-    parser.add_argument("-t", "--title", dest='title', help = "Title of note", default='Title')
-    parser.add_argument("-c", "--class", dest='classname', help = "For teaching, course number.", default="Bates PHYS")
-
+    parser.add_argument("filename", metavar='filename', type=str, help="Name of the file.")
+    parser.add_argument("-d", "--doctype", dest='doctype', help="Type of note.", default="default")
+    parser.add_argument("-t", "--title", dest='title', help="Title of note.", default='(filename)')
     args = parser.parse_args()
+
     latex_jinja_env = jinja2.Environment(
         block_start_string = '\BLOCK{',
         block_end_string = '}',
@@ -20,21 +19,21 @@ def main():
         comment_end_string = '}',
         line_statement_prefix = '%%',
         line_comment_prefix = '%#',
-        trim_blocks = True,
+        trim_blocks = False,
         autoescape = False,
-        loader = jinja2.PackageLoader('quicknote', 'templates')
-    )
+        loader = jinja2.PackageLoader('quicknote', 'templates'))
+
     filename = args.filename
+    doctype = args.doctype
+    title = args.title
+    if title == '(filename)':
+        title = filename
     if not filename.endswith('.tex'):
         filename += '.tex'
+    if not doctype.endswith('.tex'):
+        doctype += '.tex'
 
-    if args.doctype == "research":
-        print("research")
-        template = latex_jinja_env.get_template("research.tex")
-    else:
-        print("teaching.")
-        template = latex_jinja_env.get_template("teaching.tex")
-
-    out_text = template.render(title=args.title,classname=args.classname)
-    with open(filename,'w') as outfile:
+    template = latex_jinja_env.get_template(doctype)
+    out_text = template.render(title=title)
+    with open(filename, 'w') as outfile:
         outfile.write(out_text)
